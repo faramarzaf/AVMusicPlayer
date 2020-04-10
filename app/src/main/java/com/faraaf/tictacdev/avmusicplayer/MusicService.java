a99ee6824f9ed31104e7aa8d0c65f82283898aca
FaramarzAf <faramarzafzali@gmail.com>
2020-04-10 22:49:37 +0430

    some changes in service class

--- app/src/main/java/com/faraaf/tictacdev/avmusicplayer/MusicService.java
+++ app/src/main/java/com/faraaf/tictacdev/avmusicplayer/MusicService.java
@@ -1,70 +1,88 @@
 package com.faraaf.tictacdev.avmusicplayer;
 
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
-import android.os.PowerManager;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class MusicService extends Service implements
-        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
-        MediaPlayer.OnCompletionListener{
 
 
+        MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener, AudioManager.OnAudioFocusChangeListener {
 
-    //media player
+
     private MediaPlayer player;
-    //song list
-    private ArrayList<Song> songs;
-    //current position
-    private int songPosn;
-    //binder
+    private ArrayList<Song> songsList;
     private final IBinder musicBind = new MusicBinder();
-    //title of current song
-    private String songTitle="";
-    //notification id
-    private static final int NOTIFY_ID=1;
-    //shuffle flag and random
-    private boolean shuffle=false;
-    private Random rand;
+    private int currentSongIndex;
+    private AudioManager mAudioManager;
 
 
-    public void onCreate(){
-        //create the service
+    public void onCreate() {
         super.onCreate();
-        //initialize position
-        songPosn=0;
-        //random
-        rand=new Random();
-        //create player
         player = new MediaPlayer();
-        //initialize
         initMusicPlayer();
     }
 
-    public void initMusicPlayer(){
-        //set player properties
-        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        //set listeners
-        player.setOnPreparedListener(this);
+    public void initMusicPlayer() {
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (mAudioManager != null) {
+            mAudioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+        }
+
+
+        //    player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        //  player.setAudioStreamType(AudioManager.STREAM_MUSIC);
         player.setOnCompletionListener(this);
         player.setOnErrorListener(this);
     }
 
     //pass song list
-    public void setList(ArrayList<Song> theSongs){
-        songs=theSongs;
+    public void setList(ArrayList<Song> theSongs) {
+        songsList = theSongs;
+    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        switch (focusChange) {
+            case AudioManager.AUDIOFOCUS_GAIN:
+                if (!player.isPlaying())
+                    player.start();
+                break;
+
+            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
+                break;
+
+            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
+                break;
+
+            case AudioManager.AUDIOFOCUS_LOSS:
+                if (player.isPlaying())
+                    player.pause();
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                if (player.isPlaying()) {
+                    player.pause();
+                }
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                if (player.isPlaying()) {
+                    player.pause();
+                }
+                break;
+            case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
+                break;
+
+            default:
+        }
     }
 
     //binder
@@ -82,7 +100,7 @@ public class MusicService extends Service implements
 
     //release resources when unbind
     @Override
-    public boolean onUnbind(Intent intent){
+    public boolean onUnbind(Intent intent) {
         player.stop();
         player.release();
         return false;
@@ -90,39 +108,28 @@ public class MusicService extends Service implements
 
 
     //play a song
-    public void playSong(){
-        //play
-        player.reset();
-        //get song
-        Song playSong = songs.get(songPosn);
-        //get title
-        songTitle=playSong.getTitle();
-        //get id
-        long currSong = playSong.getID();
-        //set uri
-        Uri trackUri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                currSong);
-        //set the data source
-        try{
-            player.setDataSource(getApplicationContext(), trackUri);
-        }
-        catch(Exception e){
+    public void playSong(Song song) {
+        try {
+            player.reset();
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource(getApplicationContext(), Uri.parse(song.getSongLink()));
+            player.prepare();
+            player.start();
+        } catch (Exception e) {
             Log.e("MUSIC SERVICE", "Error setting data source", e);
         }
-        player.prepareAsync();
-    }
-
-    //set the song
-    public void setSong(int songIndex){
-        songPosn=songIndex;
+        //    player.prepareAsync();
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        //check if playback has reached the end of a track
-        if(player.getCurrentPosition()>0){
-            mp.reset();
-            playNext();
+
+        if (currentSongIndex < (songsList.size() - 1)) {
+            playSong(songsList.get(currentSongIndex + 1));
+            currentSongIndex = currentSongIndex + 1;
+        } else {
+            playSong(songsList.get(0));
+            currentSongIndex = 0;
         }
     }
 
@@ -133,87 +140,61 @@ public class MusicService extends Service implements
         return false;
     }
 
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        //start playback
-        mp.start();
-        //notification
-        Intent notIntent = new Intent(this, MainActivity.class);
-        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendInt = PendingIntent.getActivity(this, 0, notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification.Builder builder = new Notification.Builder(this);
-
-        builder.setContentIntent(pendInt)
-                .setSmallIcon(R.drawable.play)
-                .setTicker(songTitle)
-                .setOngoing(true)
-                .setContentTitle("Playing")
-                .setContentText(songTitle);
-        Notification not = builder.build();
-        startForeground(NOTIFY_ID, not);
-    }
-
     //playback methods
-    public int getPosn(){
+    public int getPosn() {
         return player.getCurrentPosition();
     }
 
-    public int getDur(){
+    public int getDur() {
         return player.getDuration();
     }
 
-    public boolean isPng(){
+    public boolean isPng() {
         return player.isPlaying();
     }
 
-    public void pausePlayer(){
+    public void pausePlayer() {
         player.pause();
     }
 
-    public void seek(int posn){
+    public void seek(int posn) {
         player.seekTo(posn);
     }
 
-    public void go(){
+    public void go() {
         player.start();
     }
 
 
-    //skip to previous track
-    public void playPrev(){
-        songPosn--;
-        if(songPosn<0) songPosn=songs.size()-1;
-        playSong();
+    public void playPrev() {
+        if (currentSongIndex > 0) {
+            Song song = songsList.get(currentSongIndex - 1);
+            playSong(song);
+            currentSongIndex = currentSongIndex - 1;
+        } else {
+            Song song = songsList.get(songsList.size() - 1);
+            playSong(song);
+            currentSongIndex = songsList.size() - 1;
+        }
+
     }
 
-    //skip to next
-    public void playNext(){
-        if(shuffle){
-            int newSong = songPosn;
-            while(newSong==songPosn){
-                newSong=rand.nextInt(songs.size());
-            }
-            songPosn=newSong;
+    public void playNext() {
+        if (currentSongIndex < (songsList.size() - 1)) {
+            Song song = songsList.get(currentSongIndex + 1);
+            playSong(song);
+            currentSongIndex = currentSongIndex + 1;
+        } else {
+            playSong(songsList.get(0));
+            currentSongIndex = 0;
         }
-        else{
-            songPosn++;
-            if(songPosn>=songs.size()) songPosn=0;
-        }
-        playSong();
     }
 
     @Override
     public void onDestroy() {
+        mAudioManager.abandonAudioFocus(this);
         stopForeground(true);
     }
-
-    //toggle shuffle
-    public void setShuffle(){
-        if(shuffle) shuffle=false;
-        else shuffle=true;
-    }
-
 
 
 }
